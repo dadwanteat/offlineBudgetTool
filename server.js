38c3b909e9fd3e996e3d26a2d31130ccaa6dab37
Robert Dalton <dalton.robertw@gmail.com>
2021-01-15 11:15:59 -0800

    deploying again

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/offlineBudgetTracker',
+  process.env.MONGODB_URI || 'mongodb://localhost/rwd-offline-budget-tracker',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
